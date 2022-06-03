f5a16d112538fd7cd918ed7c7e0e87e18853d5c2
Anna O <usune@mail.ru>
2022-06-04 00:46:50 +0300

    first commit

---
+++ js/task02.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const isPrime = (num) => {
+    for (let i = 2; i < num; i++ ) {
+        if (num % i === 0) {
+            return false;
+        }   
+    } 
+    
+    return true;
+};
+
+console.log(isPrime(10000019));
